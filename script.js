01fde9a27d645b1044a5ee438f400a55ad9a77a4
Nanma Krishnan E <107807504+Nanmakrishnan@users.noreply.github.com>
2024-10-13 17:38:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const chatbox = document.querySelector(".chatbox");
 const chatbotToggler = document.querySelector(".chatbot-toggler");
 const chatbotCloseBtn = document.querySelector(".close-btn");
 
-// const API_KEY = "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";
+let userMessage;
+const API_KEY="";
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
@@ -18,8 +19,8 @@ const createChatLi = (message, className) => {
     return chatLi;
 };
 
+const API_URL = "";
 const generateResponse = (incomingChatLi) => {
-    // const API_URL = "https://api.openai.com/v1/chat/completions";
     const messageElement = incomingChatLi.querySelector("p");
 
     const requestOptions = {
@@ -48,7 +49,7 @@ const generateResponse = (incomingChatLi) => {
         });
 };
 
-let userMessage;
+
 const handleChat = () => {
     userMessage = chatInput.value.trim();
     
